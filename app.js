bce20e1f6a14c3e2442133342c4fc007a16c2048
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-11 15:30:07 +0200

    Read input from file

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var caesar = require('./caesar.js');
+var fs = require('fs');
 
 if (process.argv.length !== 5) {
-  console.log('Usage: node app.js <string> <numberOfShifts> <encode|decode>');
+  console.log('Usage: node app.js <filename> <numberOfShifts> <encode|decode>');
   process.exit(1);
 }
 
 var method = caesar[process.argv[4]];
 var numberOfShifts = parseInt(process.argv[3], 10);
-var message = process.argv[2];
+var fileName = process.argv[2];
 
-console.log(method(message, numberOfShifts));
+var fileStream = fs.createReadStream(fileName, { encoding: 'utf8' });
+
+fileStream.on('readable', function () {
+  var chunk;
+  while (null !== (chunk = fileStream.read())) {
+    console.log(method(chunk, numberOfShifts));
+  }
+});
