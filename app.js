3aaf913039d50e95912265f2a9c923beee87f814
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-11 14:36:26 +0200

    Using command line params

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var caesar = require('./caesar.js');
 
-console.log("ABC encoded: " + caesar.encode("ABC", 2));
-console.log("ABC encoded and decoded: " + 
-            caesar.decode(caesar.encode("ABC", 2), 2));
+if (process.argv.length !== 5) {
+  console.log('Usage: node app.js <string> <numberOfShifts> <encode|decode>');
+  process.exit(1);
+}
+
+var method = caesar[process.argv[4]];
+var numberOfShifts = parseInt(process.argv[3], 10);
+var message = process.argv[2];
+
+console.log(method(message, numberOfShifts));
