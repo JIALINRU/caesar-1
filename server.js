9a7ab544c0c982f41aa6ece4b7490b901c540f1f
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-12 14:21:59 +0200

    Server with different routes

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var fs = require('fs');
 var caesar = require('./caesar.js');
 
 var server = http.createServer(function(request, response) {
+  var transformStream;
+  if (request.url === '/decode') {
+    transformStream = caesar.createDecodeStream(2);
+  } else if (request.url === '/encode') {
+    transformStream = caesar.createEncodeStream(2);
+  } else {
+    transformStream = require('stream').PassThrough();
+  }
+
   request
-    .pipe(caesar.createEncodeStream(2))
+    .pipe(transformStream)
     .pipe(response);
 });
 
