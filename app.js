01c83c17295f84c0bb2efa68adc6c80ed3513a24
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-12 13:23:52 +0200

    Encoding and decoding with streams

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var caesar = require('./caesar.js');
 var fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.log('Usage: node app.js <filename> <numberOfShifts> <encode|decode>');
+if (process.argv.length !== 4) {
+  console.log('Usage: node app.js <filename> <numberOfShifts>');
   process.exit(1);
 }
 
-var method = caesar[process.argv[4]];
 var numberOfShifts = parseInt(process.argv[3], 10);
 var fileName = process.argv[2];
 
-var caesarEncodeStream = caesar.createEncodeStream(2);
+var caesarEncodeStream = caesar.createEncodeStream(numberOfShifts);
+var caesarDecodeStream = caesar.createDecodeStream(numberOfShifts);
 var fileStream = fs.createReadStream(fileName, { encoding: 'utf8' });
 
 fileStream
   .pipe(caesarEncodeStream)
+  .pipe(caesarDecodeStream)
   .pipe(process.stdout);
