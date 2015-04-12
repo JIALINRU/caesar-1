eb186389b2acb33f8e3aa5b8041cf42e6c05663b
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-12 13:55:10 +0200

    Serving encoded file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var fs = require('fs');
+var caesar = require('./caesar.js');
 
 var server = http.createServer(function(request, response) {
-  response.end('Hello world');
+  fs.createReadStream('message.txt', {encoding: 'utf8' })
+    .pipe(caesar.createEncodeStream(2))
+    .pipe(response);
 });
 
 server.listen(1337);
