cf074b28a38d6922edf47c29f785f95b5644d9dd
Thomas Haukland <thomas.haukland@gmail.com>
2015-04-17 20:19:16 +0200

    Cluster

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 var http = require('http');
 var fs = require('fs');
 var caesar = require('./caesar.js');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var requestsServed = 0;
+var serverNum;
 
-var server = http.createServer(function(request, response) {
-  var transformStream;
-  if (request.url === '/decode') {
-    transformStream = caesar.createDecodeStream(2);
-  } else if (request.url === '/encode') {
-    transformStream = caesar.createEncodeStream(2);
-  } else {
-    transformStream = require('stream').PassThrough();
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork({ serverNum: i });
   }
+} else {
+  serverNum = process.env.serverNum;
+  var server = http.createServer(function(request, response) {
+    var transformStream;
+    if (request.url === '/decode') {
+      transformStream = caesar.createDecodeStream(2);
+    } else if (request.url === '/encode') {
+      transformStream = caesar.createEncodeStream(2);
+    } else {
+      transformStream = require('stream').PassThrough();
+    }
+    request
+      .pipe(transformStream)
+      .pipe(response);
+    console.log("Server: " + serverNum + ". Requests served: " + ++requestsServed);
+  });
+  server.listen(1337);
+}
 
-  request
-    .pipe(transformStream)
-    .pipe(response);
-});
-
-server.listen(1337);
